0cf5d664b017897470b787f28516f668e01378a7
Parry Huang <parryhuang94@gmail.com>
2022-09-26 13:27:09 -0400

    commit tree 8c8f62bffc3a1769b902136fb2d5b9c6796423a0

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,20 @@
 require 'pry'
+require_relative 'concerns/memorable'
+require_relative 'concerns/paramable'
 
 class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Paramable
+  include Memorable::InstanceMethods
+
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -18,19 +24,19 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
